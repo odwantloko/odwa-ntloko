58bcfb7f57b80df058fea3c0f9c07997cdb2e81e
ntlokoo <Odwa.Ntloko@vodacom.co.za>
2020-08-11 10:59:10 +0200

    removed address; added some details

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,6 +7,8 @@ class About extends Component {
       var name = this.props.data.name;
       var profilepic= "images/"+this.props.data.image;
       var bio = this.props.data.bio;
+      var hobbies = this.props.data.hobbies;
+
       var street = this.props.data.address.street;
     
       var phone= this.props.data.phone;
@@ -24,15 +26,14 @@ class About extends Component {
             <h2>About Me</h2>
 
             <p>{bio}</p>
+            <p>{hobbies}</p>
             <div className="row">
                <div className="columns contact-details">
                   <h2>Contact Details</h2>
                   <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
-                   </span><br />
-						   <span>{phone}</span><br />
+						
                      <span>{email}</span>
+                     <p>You can also contact me on any of the social media links at the bottom.</p>
 					   </p>
                </div>
                {/* <div className="columns download">
