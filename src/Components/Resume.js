553c7dfba91ca3be9f8f36106431901781b98c55
Odwa Ntloko <ntlokoo@outlook.com>
2022-03-23 19:05:40 +0200

    switched the order of the data

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -48,6 +48,17 @@ class Resume extends Component {
     return (
       <section id="resume">
 
+      <div className="row work">
+
+          <div className="three columns header-col">
+              <h1><span>Work</span></h1>
+          </div>
+
+          <div className="nine columns main-col">
+            {work}
+          </div>
+      </div>
+
       <div className="row education">
          <div className="three columns header-col">
             <h1><span>Education</span></h1>
@@ -63,16 +74,7 @@ class Resume extends Component {
       </div>
 
 
-      <div className="row work">
-
-         <div className="three columns header-col">
-            <h1><span>Work</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-          {work}
-        </div>
-    </div>
+      
 
 
 
